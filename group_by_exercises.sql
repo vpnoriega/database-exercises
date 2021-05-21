5223948d82ef82dcd933c1f499ea4a9dc16927c2
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-21 12:16:13 -0500

    Completed: Group_by_exercises

---
+++ group_by_exercises.sql
@@ -0,0 +1,51 @@
+USE employees;
+
+-- In your script, use DISTINCT to find the unique titles in the titles table. Your results should look like:
+
+SELECT DISTINCT title FROM titles;
+
+
+-- Find your query for employees whose last names start and end with 'E'.
+-- Update the query to find just the unique last names that start and end with 'E' using GROUP BY
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E'
+GROUP BY last_name
+ORDER BY last_name;
+
+
+-- Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E'
+GROUP BY first_name, last_name;
+
+
+-- Find the unique last names with a 'q' but not 'qu'.
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+
+-- Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+
+SELECT last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+
+-- Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names.
+
+SELECT gender, COUNT(gender)
+FROM employees
+WHERE first_name IN ('Irena','Vidya','Maya')
+GROUP BY gender;
