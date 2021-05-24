7571a0913a68335da25851cbcaf43afe017c72f5
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-24 10:43:25 -0500

    in progress: join_exercises

---
+++ join_exercises.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees as e
+         JOIN dept_emp as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
