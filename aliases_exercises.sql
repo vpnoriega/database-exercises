f03d5ca598c7614251487860bfa448a12f31a933
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-23 23:59:42 -0500

    in progress:

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -9,6 +9,12 @@ USE employees;
 
 
 -- Add the date of birth for each employee as 'DOB' to the query.
+-- SELECT CONCAT(last_name, ' ', first_name) AS full_name, (birth_date) AS DOB
+-- FROM employees
+--          LIMIT 10;
+
+-- Update the query to format full name to include the employee number so it is formatted as 'employee number - last name, first name'.
+
 SELECT CONCAT(last_name, ' ', first_name) AS full_name, (birth_date) AS DOB
 FROM employees
          LIMIT 10;
